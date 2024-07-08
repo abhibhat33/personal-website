2ef9061aa40f5456afd8795c081a174402c86fa6
Abhishek <abhishek@tikkl.com>
2024-07-08 13:51:07 +0530

    fixed base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/personal-website/",
   plugins: [
     remix({
+      basename: "/personal-website/",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
